96852fad630488997ea6cf9021b9d2e8d1fd322c
Wayne Daniels <wa.daniels@hotmail.com>
2020-02-06 21:29:58 -0800

    GradeForm Component Added

--- components/grade-form.js
+++ components/grade-form.js
@@ -2,7 +2,7 @@ class GradeForm{
   constructor(formElement){
     this.formElement = formElement;
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.formElement
+    this.formElement.addEventListener('submit', this.handleSubmit);
   }
   onSubmit(createGrade){
     this.createGrade = createGrade;
--- main.js
+++ main.js
@@ -4,5 +4,8 @@ var gradeTable = new GradeTable(table);
 var hdr = document.querySelector('header');
 var pageHeader = new PageHeader(hdr);
 
-var app = new App(gradeTable, pageHeader);
+var gForm = document.querySelector('form');
+var gradeForm = new GradeForm(gForm);
+
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
