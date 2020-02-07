40b6b3e046784bbffa696f24e53ba852dfbeb6c0
Wayne Daniels <wa.daniels@hotmail.com>
2020-02-06 21:06:21 -0800

    Completed HTML Mods

---
+++ components/grade-form.js
@@ -0,0 +1,15 @@
+class GradeForm{
+  constructor(formElement){
+    this.formElement = formElement;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement
+  }
+  onSubmit(createGrade){
+    this.createGrade = createGrade;
+  }
+  handleSubmit(event){
+    event = preventDefault();
+    console.log("hi");
+
+  }
+}
